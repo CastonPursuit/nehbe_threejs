0f63bf88accb91459cc33a5e3cd376ebee85765e
CLeeBenjamin <castonboyd@gmail.com>
2024-01-30 22:57:52 -0500

    Update dependencies and add animation to Home component

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,4 +1,12 @@
-import React from 'react'
+import { motion, AnimatePresence } from 'framer-motion';
+
+import state from '../store';
+import {
+  headContainerAnimation,
+  headTextAnimation,
+  headContentAnimation,
+  slideAnimation
+} from '../config/motion';
 
 export const Home = () => {
   return (
